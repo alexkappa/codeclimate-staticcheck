779e65ca457572400364b6d5969aa37c678374b9
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2017-08-06 10:13:07 +0200

    WiP: still getting import errors

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,6 @@
 FROM alpine:3.4
 MAINTAINER Alex Kalyvitis <alex.kalyvitis@yieldr.com>
-WORKDIR /usr/src/app
-COPY codeclimate-staticcheck.go /usr/src/app/codeclimate-staticcheck.go
-ADD engine.json /
-RUN apk --update add go git && \
-  export GOPATH=/tmp/go GOBIN=/usr/local/bin && \
-  go get -d . && \
-  go install codeclimate-staticcheck.go && \
-  apk del go git && \
-  rm -rf "$GOPATH" && \
-  rm /var/cache/apk/*
+COPY bin/codeclimate-staticcheck-linux-amd64 /usr/local/bin/codeclimate-staticcheck
 WORKDIR /code
 VOLUME /code
 RUN adduser -u 9000 -D app
--- codeclimate-staticcheck.go
+++ codeclimate-staticcheck.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"go/parser"
 	"os"
 
 	"github.com/codeclimate/cc-engine-go/engine"
 	"golang.org/x/tools/go/loader"
-	"honnef.co/go/tools/lint"
+	"honnef.co/go/tools/lint/lintutil"
 	"honnef.co/go/tools/staticcheck"
 )
 
@@ -20,23 +19,35 @@ func main() {
 
 	path := "/code/"
 
-	loader := &loader.Config{ParserMode: parser.ImportsOnly, Cwd: path}
-	loader.CreateFromFilenames(path, engine.IncludePaths(path, config)...)
-	program, err := loader.Load()
+	filenames, err := engine.GoFileWalk(path, engine.IncludePaths(path, config))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(os.Stderr, "Error reading files: %s", err)
 		os.Exit(1)
 	}
 
+	loader := &loader.Config{ParserMode: 0, Cwd: path}
+	loader.CreateFromFilenames(path, filenames...)
+	program, err := loader.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error loading program: %s", err)
+		os.Exit(1)
+	}
+
+	var packages []string
+	for _, packageInfo := range program.InitialPackages() {
+		packages = append(packages, packageInfo.Pkg.Name())
+	}
+
 	checker := staticcheck.NewChecker()
-	linter := lint.Linter{Checker: checker}
-	problems := linter.Lint(program)
+	problems, _, err := lintutil.Lint(checker, packages, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error linting program: %s", err)
+		os.Exit(1)
+	}
 
 	for _, problem := range problems {
-
 		position := program.Fset.Position(problem.Position)
-
-		engine.PrintIssue(&engine.Issue{
+		issue := &engine.Issue{
 			Type:              "issue",
 			Check:             "Staticcheck" + problem.Text,
 			Description:       problem.Text,
@@ -48,6 +59,7 @@ func main() {
 				},
 			},
 			Categories: []string{"Style"},
-		})
+		}
+		engine.PrintIssue(issue)
 	}
 }
