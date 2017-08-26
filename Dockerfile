81024762d45b7bb00ddf704640b30e5bba330482
Alex Kalyvitis <alex.kalyvitis@gmail.com>
2017-08-26 14:18:06 +0200

    working prototype with support for subpackages

--- Dockerfile
+++ Dockerfile
@@ -5,12 +5,12 @@ MAINTAINER Alex Kalyvitis <alex.kalyvitis@yieldr.com>
 WORKDIR /code
 VOLUME /code
 
-COPY bin/codeclimate-staticcheck-linux-amd64 /usr/local/bin/codeclimate-staticcheck
+COPY . /go/src/app
 
-# RUN set -x \
-# 	&& cd /go/src/app \
-# 	&& go build -o codeclimate-staticcheck \
-# 	&& cp codeclimate-staticcheck /usr/local/bin
+RUN set -x \
+	&& cd /go/src/app \
+	&& go build -o codeclimate-staticcheck \
+	&& cp codeclimate-staticcheck /usr/local/bin
 
 RUN set -x \
 	&& useradd -u 9000 app \
--- codeclimate-staticcheck.go
+++ codeclimate-staticcheck.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"go/token"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/alexkappa/codeclimate-staticcheck/fileutil"
-	"github.com/codeclimate/cc-engine-go/engine"
-	"golang.org/x/tools/go/loader"
-	"honnef.co/go/tools/lint"
+	"honnef.co/go/tools/lint/lintutil"
 	"honnef.co/go/tools/staticcheck"
+
+	"github.com/codeclimate/cc-engine-go/engine"
+	"github.com/kisielk/gotool"
 )
 
 func main() {
@@ -25,69 +27,122 @@ func main() {
 
 	pkgPath := filepath.Join(os.Getenv("GOPATH"), "src", "app")
 	if engineConfig, ok := config["config"].(map[string]interface{}); ok {
-		if packageDir, ok := engineConfig["package_dir"].(string); ok {
+		if packageDir, ok := engineConfig["package"].(string); ok {
 			pkgPath = filepath.Join(os.Getenv("GOPATH"), "src", packageDir)
 		}
 	}
 
-	err = fileutil.CopyDir(pkgPath, rootPath)
+	err = copyDir(pkgPath, rootPath)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Failed copying dir to %q. %s\n", pkgPath, err)
 		os.Exit(1)
 	}
 
-	filenames, err := fileutil.GoFileWalk(pkgPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading files: %s\n", err)
-		os.Exit(1)
-	}
+	os.Chdir(pkgPath)
 
-	loader := &loader.Config{
-		ParserMode:  0,
-		Cwd:         pkgPath,
-		AllowErrors: true,
-	}
-	loader.CreateFromFilenames(pkgPath, filenames...)
-	program, err := loader.Load()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error loading program: %s\n", err)
-		os.Exit(1)
-	}
+	pkgs := gotool.ImportPaths([]string{"./..."})
 
 	checker := staticcheck.NewChecker()
-	linter := lint.Linter{Checker: checker}
-	problems := linter.Lint(program)
+	problems, program, err := lintutil.Lint(checker, pkgs, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error linting program: %s\n", err)
+		os.Exit(1)
+	}
+
+	includePaths := engine.IncludePaths(pkgPath, config)
 
 	for _, problem := range problems {
 
-		issue := &engine.Issue{
-			Type:              "issue",
-			Check:             check(problem.Text),
-			Description:       description(problem.Text),
-			RemediationPoints: 5000,
-			Location:          location(program, problem.Position, rootPath),
-			Categories:        []string{"Style"},
-		}
+		position := program.Fset.Position(problem.Position)
 
-		engine.PrintIssue(issue)
+		if inSlice(position.Filename, includePaths) {
+			issue := &engine.Issue{
+				Type:              "issue",
+				Check:             check(problem.Text),
+				Description:       description(problem.Text),
+				RemediationPoints: 5000,
+				Location:          location(position, pkgPath),
+				Categories:        []string{"Style"},
+			}
+
+			engine.PrintIssue(issue)
+		}
 	}
 }
 
 func check(s string) string {
-	return "Staticcheck/" + strings.TrimRight(strings.Split(s, "(")[1], ")")
+	return "Go-StaticCheck/" + strings.TrimRight(strings.Split(s, "(")[1], ")")
 }
 
 func description(s string) string {
 	return strings.Split(s, "(")[0]
 }
 
-func location(p *loader.Program, pos token.Pos, path string) *engine.Location {
-	position := p.Fset.Position(pos)
+func location(pos token.Position, path string) *engine.Location {
 	return &engine.Location{
-		Path: strings.SplitAfter(position.Filename, path)[1],
+		Path: strings.SplitAfter(pos.Filename, path+"/")[1],
 		Lines: &engine.LinesOnlyPosition{
-			Begin: position.Line,
-			End:   position.Line,
+			Begin: pos.Line,
+			End:   pos.Line,
 		},
 	}
+}
+
+func copyDir(dst, src string) error {
+	walkFn := func(srcPath string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		relPath, err := filepath.Rel(src, srcPath)
+		if err != nil {
+			return fmt.Errorf("failed getting relative path. %s", err)
+		}
+		dstPath := filepath.Join(dst, relPath)
+		if info.IsDir() {
+			return os.MkdirAll(dstPath, info.Mode())
+		}
+		return copyFile(dstPath, srcPath)
+	}
+	return filepath.Walk(src, walkFn)
+}
+
+func copyFile(dst, src string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	tmp, err := ioutil.TempFile(filepath.Dir(dst), filepath.Base(dst)+".")
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(tmp, in)
+	if err != nil {
+		tmp.Close()
+		os.Remove(tmp.Name())
+		return err
+	}
+	if err := tmp.Close(); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+	const perm = 0644
+	if err := os.Chmod(tmp.Name(), perm); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+	if err := os.Rename(tmp.Name(), dst); err != nil {
+		os.Remove(tmp.Name())
+		return err
+	}
+	return nil
+}
+
+func inSlice(s string, slice []string) bool {
+	for _, elem := range slice {
+		if elem == s {
+			return true
+		}
+	}
+	return false
 }
